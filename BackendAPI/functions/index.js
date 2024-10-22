0b6cbb02718ab2cbceecf9c778aea64bef9e8587
PaigeRussell <104575754+PaigeRU@users.noreply.github.com>
2024-10-22 20:06:21 +0200

    updated code

--- BackendAPI/functions/index.js
+++ BackendAPI/functions/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const functions = require('firebase-functions');
+require('dotenv').config(); // Load environment variables from .env file
 
 // Initialize Express app
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+}));
 app.use(express.json());
+// Access the JWT secret from .env file
+const jwtSecret = process.env.JWT_SECRET || 'default_secret'; // Fallback to a default secret if not set
 
-// Access the JWT secret from Firebase config
-const jwtSecret = functions.config().jwt.secret;
-
-// Register your routes after Firebase is initialized
+// Register your routes
 const authRoutes = require('./routes/authRoutes.js');
 const appointmentRoutes = require('./routes/appointmentRoutes.js');
 
